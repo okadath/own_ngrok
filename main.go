e029d26d4ac0d1c5ad297dd7a2dab2f25998f4f0
okadath <vintaw.01@gmail.com>
2021-10-24 16:31:08 -0500

    to  server

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 func main() {
 	var port = flag.String("p", "9999", "server port to use")
 	var host = flag.String("h", "159.89.134.107", "server hostname to use")
-	var addr = flag.String("b", "127.0.0.1", "ip to bind [server only]")
+	var addr = flag.String("b", "159.89.134.107", "ip to bind [server only]")
 	flag.Parse()
 
 	// client usage: groktunnel [-h=<server hostname>] <local port>
